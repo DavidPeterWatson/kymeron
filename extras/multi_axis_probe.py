5af8dbc33e1cba47bb97208eeab0daf08e8f28bd
David Watson <davidpeterwatson@gmail.com>
2024-12-27 14:56:27 +1300

    replace pull down

--- extras/multi_axis_probe.py
+++ extras/multi_axis_probe.py
@@ -514,7 +514,7 @@ class MultiAxisRockingProbe:
         pin = config.get('pin')
         buttons = self.printer.load_object(config, 'buttons')
         ppins = self.printer.lookup_object('pins')
-        ppins.allow_multi_use_pin(pin.replace('^', '').replace('!', ''))
+        ppins.allow_multi_use_pin(pin.replace('^', '').replace('!', '').replace('~', ''))
         buttons.register_buttons([pin], self._button_handler)
 
         self.mcu_probes = [
