ef78066203ac5cc47c290b61b57e2776ce17a0b8
David Watson <davidpeterwatson@gmail.com>
2024-12-27 15:36:20 +1300

    try again

--- extras/tool_probe.py
+++ extras/tool_probe.py
@@ -13,9 +13,9 @@ class ToolProbe:
         # # self.probe = self.printer.lookup_object(self.probe_name)
 
         # self.travel_speed = config.getfloat('travel_speed', 10.0, above=0.)
-        # self.x_pos = config.getfloat('x_pos')
-        # self.y_pos = config.getfloat('y_pos')
-        # self.z_pos = config.getfloat('z_pos')
+        self.x_pos = config.getfloat('x_pos')
+        self.y_pos = config.getfloat('y_pos')
+        self.z_pos = config.getfloat('z_pos')
 
         # self.spread = config.getfloat('spread', 5.0)
         # self.lower_z = config.getfloat('lower_z', 0.5)
