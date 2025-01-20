55fea7c6101da44039177d61561f4588c9aace47
David Watson <davidpeterwatson@gmail.com>
2025-01-20 21:18:36 +1300

    EmergencyStop

--- extras/emergency_stop.py
+++ extras/emergency_stop.py
@@ -1,48 +1,44 @@
+import logging
+
 class EmergencyStop:
     def __init__(self, config):
         self.printer = config.get_printer()
-        gcode = self.printer.lookup_object('gcode')
-        try:
-            self.name = config.get_name().split(' ')[-1]
-            self.buttons = self.printer.load_object(config, "buttons")
-
-            # Register kill button
-            self.register_button(config, 'kill_pin', self.kill_callback)
-            self.printer.add_object('emergency_stop_' + self.name, self)
-        except Exception as e:
-            reason = str(e)
-            gcode.respond_info(f"Error initialising emergency button. {reason}")
-            # raise e
-
-
-    def register_button(self, config, name, callback):
-        gcode = self.printer.lookup_object('gcode')
-        try:
-            pin = config.get(name, None)
-            if pin is None:
-                return
-            # buttons = self.printer.lookup_object("buttons")
-            if config.get('analog_range', None) is None:
-                self.buttons.register_buttons([pin], callback)
-                return
+        self.name = config.get_name().split(' ')[-1]
+        self.pin = config.get('pin')
+        self.last_state = 0
+        buttons = self.printer.load_object(config, "buttons")
+        if config.get('analog_range', None) is None:
+            buttons.register_buttons([self.pin], self.button_callback)
+        else:
             amin, amax = config.getfloatlist('analog_range', count=2)
             pullup = config.getfloat('analog_pullup_resistor', 4700., above=0.)
-            self.buttons.register_adc_button(pin, amin, amax, pullup, callback)
-        except Exception as e:
-            reason = str(e)
-            gcode.respond_info(f"Error registering emergency button. {reason}")
-            raise e
+            buttons.register_adc_button(self.pin, amin, amax, pullup,
+                                        self.button_callback)
 
+        self.gcode = self.printer.lookup_object('gcode')
+        self.gcode.register_mux_command("QUERY_EMERGENCY", "BUTTON", self.name,
+                                        self.cmd_QUERY_BUTTON,
+                                        desc=self.cmd_QUERY_BUTTON_help)
 
-    def kill_callback(self, eventtime):
+    cmd_QUERY_BUTTON_help = "Report on the state of a button"
+    def cmd_QUERY_BUTTON(self, gcmd):
+        gcmd.respond_info(self.name + ": " + self.get_status()['state'])
+
+    def button_callback(self, eventtime, state):
+        self.last_state = state
         try:
             gcode = self.printer.lookup_object('gcode')
-            gcode.respond_info("emergency stop activated!")
-            # self.printer.invoke_shutdown("Shutdown due to emergency stop!")
-        except Exception as e:
-            reason = str(e)
-            gcode.respond_info(f"Error calling kill. {reason}")
-        
+            if state:
+                gcode.respond_info("emergency stop activated!")
+            else:
+                gcode.respond_info("emergency stop deactivated!")
+        except:
+            logging.exception("Script running error")
+
+    def get_status(self, eventtime=None):
+        if self.last_state:
+            return {'state': "PRESSED"}
+        return {'state': "RELEASED"}
 
 def load_config_prefix(config):
     return EmergencyStop(config)
