dc8017ea0323c36b6045b6a5dfc044b9136a3c43
David Watson <davidpeterwatson@gmail.com>
2025-01-22 10:27:33 +1300

    unsafe zone

--- extras/dock.py
+++ extras/dock.py
@@ -6,7 +6,8 @@ class Dock:
         self.safe_y = float(config.get('safe_y') or 0)
         self.load_yd = float(config.get('load_yd') or 0)
         self.engage_xd = float(config.get('engage_xd') or 0)
-        self.safe_zone_button = config.get('safe_zone_button')
+        self.unsafe_zone = config.get('unsafe_zone')
+        # self.safe_zone_button = config.get('safe_zone_button')
         self.printer.add_object('dock ' + self.name, self)
 
 
