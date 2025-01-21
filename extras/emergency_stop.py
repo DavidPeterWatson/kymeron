99d2fc67249bec91571a8d6e7957f4e38ce64f46
David Watson <davidpeterwatson@gmail.com>
2025-01-22 09:21:23 +1300

    Probe tune

--- extras/emergency_stop.py
+++ extras/emergency_stop.py
@@ -5,7 +5,7 @@ class EmergencyStop:
         self.printer = config.get_printer()
         self.name = config.get_name().split(' ')[-1]
         self.pin = config.get('pin')
-        self.enabled = config.get('enabled', True)
+        self.enabled = config.getboolean('enabled', True)
         self.last_state = 0
         buttons = self.printer.load_object(config, "buttons")
         if config.get('analog_range', None) is None:
