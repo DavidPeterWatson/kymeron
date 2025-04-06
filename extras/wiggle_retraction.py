3ec427f5b54299556fdbdf8db65bf7d2db5aa2d8
David Watson <davidpeterwatson@gmail.com>
2025-04-06 18:17:13 +1200

    Refactor retraction parameters and G-code macros for improved consistency
    - Commented out unused retraction parameters in wiggle_retraction.py to streamline configuration.
    - Updated retraction.cfg with new values for hot zone retract length and speed, enhancing performance.
    - Modified G-code macros to reference the new firmware_retraction settings, ensuring consistent application across retraction operations.

--- extras/wiggle_retraction.py
+++ extras/wiggle_retraction.py
@@ -5,11 +5,11 @@ class WiggleRetraction:
         self.hot_zone_retract_speed = float(config.get('hot_zone_retract_speed') or 80)
         self.retract_xy_wiggle = float(config.get('retract_xy_wiggle') or 2)
         self.retract_z = float(config.get('retract_z') or 0.1)
-        self.retract_length = float(config.get('retract_length') or 3)
-        self.unretract_extra_length = float(config.get('unretract_extra_length') or 0.0)
-        self.retract_speed = float(config.get('retract_speed') or 100)
+        # self.retract_length = float(config.get('retract_length') or 3)
+        # self.unretract_extra_length = float(config.get('unretract_extra_length') or 0.0)
+        # self.retract_speed = float(config.get('retract_speed') or 100)
         self.unretract_dwell = float(config.get('unretract_dwell') or 0.1)
-        self.unretract_speed = float(config.get('unretract_speed') or 100)
+        # self.unretract_speed = float(config.get('unretract_speed') or 100)
         self.printer.add_object('retraction', self)
 
 
